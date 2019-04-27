c50d5ef77a44cd877b125a82c77a040bfa7d15b5
Thorbjörn Sundragon <puggan@spiro.se>
2019-04-27 20:31:53 +0200

    guest(), isAjax(), redirect()

--- src/main/java/Index.java
+++ src/main/java/Index.java
@@ -1,16 +1,61 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 
 @Controller
 @EnableAutoConfiguration
 public class Index
 {
+    private boolean guest(
+        HttpServletRequest request
+    ) {
+        if (request == null)
+        {
+            return true;
+        }
+
+        for(Cookie c : request.getCookies())
+        {
+            if(c.getName().equals("session_id")) {
+                return !c.getValue().equals("qwerty12345");
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isAjax(
+        HttpServletRequest request
+    ) {
+        return "XMLHttpRequest".equals(request.getHeader("X-Requested-With"));
+    }
+
+    private String redirect(
+        String url,
+        String content,
+        HttpServletResponse response,
+        HttpServletRequest request
+    ) {
+        if(!isAjax(request)) {
+            response.setStatus(302);
+            response.addHeader("Location", url);
+        }
+        return content;
+    }
+
     @RequestMapping("/list")
     @ResponseBody
     public DTResponse<Row> list(@RequestParam(defaultValue = "0") int draw, @RequestParam(name="search[value]", defaultValue = "") String search)
