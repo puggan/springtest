d87deaa0e1e5507b06ee4a13798a462e3a1d662a
Thorbjörn Sundragon <puggan@spiro.se>
2019-04-27 20:28:05 +0200

    public resources, prepare for login

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -1,7 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const cl = document.body.classList;
     const table = $(document.getElementById("t"));
     const options = {
-       ajax: "/list",
+       ajax: (data, callback, oSettings) => $.ajax(
+           {
+               "cache": false,
+               "data": data,
+               "dataType": "json",
+               "error": () => {
+                   cl.remove('loading');
+                   cl.add('auth');
+               },
+               "success": (json) => {
+                   cl.remove('loading');
+                   cl.remove('auth');
+                   oSettings.json = json;
+                   callback( json );
+               },
+               "type": oSettings.sServerMethod,
+               "url": "/list",
+           }
+       ),
        serverSide: true,
     };
     table.DataTable(options);
