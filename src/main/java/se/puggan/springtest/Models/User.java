b16ee2abfbddec726a9232fb8eab4ab7609f4774
Thorbjörn Sundragon <puggan@spiro.se>
2019-04-28 11:21:52 +0200

    JPA add indexes

--- src/main/java/se/puggan/springtest/Models/User.java
+++ src/main/java/se/puggan/springtest/Models/User.java
@@ -1,20 +1,30 @@
 package se.puggan.springtest.Models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.Set;
 
 @Entity
+@Table(indexes={
+    @Index(columnList="username", unique=true)
+})
 public class User
 {
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     public Integer id;
+    @Column(nullable = false)
     public String username;
+    @Column(nullable = false)
     public String displayname;
 
     @OneToMany
+    @JoinColumn
     private Set<UserAuth> auths;
 }
--- src/main/java/se/puggan/springtest/Models/UserAuth.java
+++ src/main/java/se/puggan/springtest/Models/UserAuth.java
@@ -2,21 +2,29 @@ package se.puggan.springtest.Models;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(indexes={
+    @Index(columnList="user_id, authtype")
+})
 public class UserAuth
 {
     public static final String PASSWORD = "password";
 
     @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
     public Integer id;
+    @Column(nullable = false)
     public String authtype;
+    @Column(nullable = false)
     public String secret;
 
     @ManyToOne(fetch = FetchType.LAZY)
