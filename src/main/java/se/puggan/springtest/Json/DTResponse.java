1f1a5d42dbf1a889d18b7345874b41f18d1d2da7
Thorbjörn Sundragon <puggan@spiro.se>
2019-05-14 08:53:53 +0200

    DTResponse numbers as long, added setList() and setPage()

--- src/main/java/se/puggan/springtest/Json/DTResponse.java
+++ src/main/java/se/puggan/springtest/Json/DTResponse.java
@@ -1,16 +1,31 @@
 package se.puggan.springtest.Json;
 
+import org.springframework.data.domain.Page;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class DTResponse<T>
 {
     public boolean ok = true;
     public int draw;
-    public int recordsTotal;
-    public int recordsFiltered;
+    public long recordsTotal;
+    public long recordsFiltered;
     public Iterable<T> data;
 
     public DTResponse() {
         data = new ArrayList<>();
+    }
+
+    public void setList(List<T> list)
+    {
+        data = list;
+        recordsFiltered = list.size();
+    }
+
+    public void setPage(Page<T> page)
+    {
+        data = page.getContent();
+        recordsFiltered = page.getTotalElements();
     }
 }
